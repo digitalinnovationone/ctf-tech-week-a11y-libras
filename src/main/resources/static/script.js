89ff67e78b5356af486be8a21c4b0c505caaa843
Venilton FalvoJr <falvojr@gmail.com>
2024-01-23 16:42:23 -0300

    App Responsivo do Renanzinho do Async/Await

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,61 +5,51 @@ const recordButton = document.getElementById('recordButton');
 const sendButton = document.getElementById('sendButton');
 const transcription = document.getElementById('transcription');
 
-recordButton.addEventListener('click', () => {
+recordButton.addEventListener('click', async () => {
     if (recordButton.textContent === 'Gravar Áudio') {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(stream => {
-                mediaRecorder = new MediaRecorder(stream);
-                mediaRecorder.start();
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
 
-                const audioChunks = [];
-                mediaRecorder.addEventListener('dataavailable', event => {
-                    audioChunks.push(event.data);
-                });
-
-                mediaRecorder.addEventListener('stop', () => {
-                    audioBlob = new Blob(audioChunks);
-                    sendButton.disabled = false;
-                });
-
-                recordButton.textContent = 'Parar Gravação';
+            const audioChunks = [];
+            mediaRecorder.addEventListener('dataavailable', event => {
+                audioChunks.push(event.data);
             });
+
+            mediaRecorder.addEventListener('stop', () => {
+                audioBlob = new Blob(audioChunks);
+                sendButton.disabled = false;
+            });
+
+            recordButton.textContent = 'Parar Gravação';
+        } catch (error) {
+            console.error('Erro ao acessar o dispositivo de mídia: ', error);
+        }
     } else {
         mediaRecorder.stop();
         recordButton.textContent = 'Gravar Áudio';
     }
 });
 
-sendButton.addEventListener('click', () => {
-    const formData = new FormData();
-    formData.append('file', audioBlob, 'audio.webm');
+sendButton.addEventListener('click', async () => {
+    try {
+        const formData = new FormData();
+        formData.append('file', audioBlob, 'audio.webm');
 
-    fetch(`http://localhost:8080/transcribed-audios`, {
-        method: 'POST',
-        body: formData,
-    }).then(response => {
+        const response = await fetch(`http://localhost:8080/transcribed-audios`, {
+            method: 'POST',
+            body: formData,
+        });
+
         if (!response.ok) {
-            return response.text().then(text => {
-                throw new Error(text || 'Ocorreu um erro desconhecido.');
-            })
+            const errorText = await response.text();
+            throw new Error(errorText || 'Ocorreu um erro desconhecido.');
         }
-        return response.json();
-    })
-    .then(data => {
-        transcription.textContent = data.transcript
-    })
-    .catch(error => {
-        console.error('Houve um problema com a operação de fetch: ', error);
-    })
-});
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, err => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    });
-}
+        const data = await response.json();
+        transcription.textContent = data.transcript;
+    } catch (error) {
+        console.error('Houve um problema com a operação de fetch: ', error);
+    }
+});
