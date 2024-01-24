a7c54e99992e173cadcd87b15df281fbb7424767
Venilton FalvoJr <falvojr@gmail.com>
2024-01-24 18:04:24 -0300

    TODOs pra live 🤟

--- src/main/resources/static/js/audio-recorder.model.js
+++ src/main/resources/static/js/audio-recorder.model.js
@@ -4,70 +4,16 @@
  * @overview https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
 class AudioRecorder {
-    #mediaRecorder = null;
-    #audioPromise = null;
-    static AUDIO_TYPE = 'audio/wav';
 
-    /**
-     * Inicia a gravação de áudio.
-     */
-    async start() {
-        const stream = await this.#requestMediaRecorderPermission();
-        this.#setupMediaRecorder(stream);
-        this.#mediaRecorder.start();
+    start() {
+        // TODO: 1.1. Iniciar a gravação de áudio
     }
 
-    /**
-     * Solicita permissão para usar o microfone e retorna o stream de mídia.
-     */
-    async #requestMediaRecorderPermission() {
-        try {
-            return await navigator.mediaDevices.getUserMedia({ audio: true });
-        } catch (error) {
-            throw new Error('Permissão de microfone negada.');
-        }
-    }
-
-    /**
-     * Configura o MediaRecorder com o stream fornecido.
-     */
-    #setupMediaRecorder(stream) {
-        this.#mediaRecorder = new MediaRecorder(stream);
-        const audioChunks = [];
-
-        this.#mediaRecorder.ondataavailable = event => audioChunks.push(event.data);
-
-        this.#audioPromise = new Promise((resolve) => {
-            this.#mediaRecorder.onstop = () => {
-                const audioBlob = new Blob(audioChunks, { type: AudioRecorder.AUDIO_TYPE });
-                stream.getTracks().forEach(track => track.stop()); // Liberando recursos
-                resolve(audioBlob);
-            };
-        });
-    }
-
-    /**
-     * Para a gravação.
-     */
     stop() {
-        if (!this.#mediaRecorder || !this.#audioPromise) {
-            throw new Error('Gravação não iniciada!');
-        }
-        try {
-            this.#mediaRecorder.stop();
-        } catch (error) {
-            console.error('Erro ao parar a gravação:', error);
-        }
-        return this.#audioPromise;
+        // TODO: 1.2. Parar a gravação de áudio.
     }
 
-    /**
-     * Retorna a Promise com o blob de áudio da gravação atual.
-     */
-    get currentRecord() {
-        if (!this.#audioPromise) {
-            throw new Error('Gravação não iniciada!');
-        }
-        return this.#audioPromise;
+    get recordedAudio() {
+        // TODO: 1.3. Retornar a última gravação de áudio.
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -21,9 +21,8 @@ const transcriptText = document.getElementById('transcriptText');
 
 // endregion
 
-async function main() {
+function main() {
     const audioRecorder = new AudioRecorder();
-
     startStopRecordButton.onclick = () => handleRecordButton(audioRecorder);
     transcribeButton.onclick = () => handleTranscribeButton(audioRecorder);
 }
@@ -37,49 +36,27 @@ async function handleRecordButton(audioRecorder) {
 }
 
 async function startRecording(audioRecorder) {
-    await audioRecorder.start();
+    // TODO: 2.1. Utilizar a instância de AudioRecorder para iniciar a gravação do áudio.
     startStopRecordButton.innerText = RECORD_BUTTON_TEXT.STOP;
     transcribeButton.disabled = true;
 }
 
 async function stopRecording(audioRecorder) {
-    const audioBlob = await audioRecorder.stop();
-    const oldAudioUrl = audioPlayer.src;
-
-    // Criar nova URL para o novo Blob
-    audioPlayer.src = URL.createObjectURL(audioBlob);
-
-    // Liberar a URL antiga, se existir
-    if (oldAudioUrl) {
-        URL.revokeObjectURL(oldAudioUrl);
-    }
-
-    // Atualizar a UI
+    // TODO: 2.2. Utilizar a instância de AudioRecorder para parar a gravação do áudio.
     startStopRecordButton.innerText = RECORD_BUTTON_TEXT.START;
     transcribeButton.disabled = false;
 }
 
 async function handleTranscribeButton(audioRecorder) {
-    const audioBlob = await audioRecorder.currentRecord;
-    const formData = new FormData();
-    formData.append('file', audioBlob, 'audio.wav');
-
+    // TODO: 2.3. Utilizar a instância de AudioRecorder para recuperar a áudio gravado.
     transcribeButton.disabled = true;
     transcribeButton.innerText = TRANSCRIBE_BUTTON_TEXT.TRANSCRIBING;
 
-    try {
-        const response = await fetch(TRANSCRIPTION_API_URL, {
-            method: 'POST',
-            body: formData,
-        });
-        const data = await response.json();
-        transcriptText.innerText = response.ok ? data.transcript : `[API ERROR] ${data.message}`;
-    } catch (error) {
-        console.error('Erro inesperado ao transcrever o áudio:', error);
-    } finally {
+    // TODO: 2.4. Integrar com nossa API REST de trancrição de áudio. Remover esse setTimeout maroto ;)
+    setTimeout(function () {
         transcribeButton.disabled = false;
         transcribeButton.innerText = TRANSCRIBE_BUTTON_TEXT.DEFAULT;
-    }
+    }, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', main);
